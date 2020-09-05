c43d622e33f384c4c0f27f521d208eaad5d2a623
PhyrosAlpha <felipe_rsilva@hotmail.com>
2020-09-05 16:54:51 -0300

    A funcionalidade de calcular a nota foi incluída, mas apresenta erro ao clicar o botão calcular quando não há notas inseridas.

--- app/src/main/java/com/example/mediadasnotas/MainActivity.java
+++ app/src/main/java/com/example/mediadasnotas/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mediadasnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void calcular_media(View view){
+        EditText inputNotaOne = findViewById(R.id.inputNotaOne);
+        EditText inputNotaTwo = findViewById(R.id.inputNotaTwo);
+        EditText inputNotaThree = findViewById(R.id.inputNotaThree);
+        TextView resultadoNotaText = findViewById(R.id.resultNumber);
+
+        double notaOne = Double.parseDouble(inputNotaOne.getText().toString());
+        double notaTwo = Double.parseDouble(inputNotaTwo.getText().toString());
+        double notaThree = Double.parseDouble(inputNotaThree.getText().toString());
+
+        String resultado = Float.toString((float) calcularMedia(notaOne, notaTwo, notaThree));
+
+        resultadoNotaText.setText(resultado);
+
+    }
+
+    private double calcularMedia(double notaOne, double notaTwo, double notaThree){
+        double valor = notaOne + notaTwo + notaThree;
+        return valor / 3;
+    }
+
 }
